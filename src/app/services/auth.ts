50a41d643ddf70cd0ca0604455846f3c54506ddc
smitimishra <Smriti.mishra@peopleprocessteck.com>
2026-09-15 13:23:00 +0530

    pull code

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -2,7 +2,7 @@ import { Injectable, NgZone, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 
-const LOGIN_PORTAL_URL = 'http://192.168.29.218:8200/welcome';
+const LOGIN_PORTAL_URL = 'http://192.168.29.216:8200/welcome';
 
 const POLL_INTERVAL_MS = 15_000;
 const MOUSE_DEBOUNCE_MS = 5_000;
@@ -14,7 +14,7 @@ export class AuthService implements OnDestroy {
   // BACKEND API
 
   private apiUrl =
-    'http://192.168.29.218:3001/api/auth';
+    'http://192.168.29.216:3001/api/auth';
 
   // SESSION STATE
 
